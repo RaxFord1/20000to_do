7932f8f330fdd41e617417b5da748be770d905d3
ag047 <54453888+ag047@users.noreply.github.com>
2019-12-25 12:56:47 +0200

    Update to_do.py

--- to_do.py
+++ to_do.py
@@ -15,7 +15,9 @@ api_hash = 'bc2fbb95a2983a6add248370f6fe7a49'
 loop = asyncio.get_event_loop()
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
                     level=logging.WARNING)
-
+                    
+                    
+#ПОДПИСКИ
 async def s(channel):
 
  #   channel = await client.get_input_entity(chat)
@@ -40,46 +42,187 @@ async def s(channel):
     
     await client(JoinChannelRequest(channel))
 
-while True:
+#ПРОСМОТРЫ ПОСТОВ
+async def v_p(channel, msg_id):
     
-    try:
+    await client(GetMessagesViewsRequest(
+        peer = channel,
+        id = [msg_id],
+        increment = True
+        )
+        )
+#ПРОСМОТРЫ КАНАЛОВ
+async def v_c(channel):
+    
+    messages = await client.get_messages(channel)
+    
+    messages_id = []
+    
+    for x in range(0, len(messages)):
         
-        target = coll_to_do.find_one({'date': datetime.now().strftime('%Y-%m-%d %H:%M:%S')})
+        messages_id.add(messages[x].id)
+    
+    await client(GetMessagesViewsRequest(
+        peer = channel,
+        id = [messages_id],
+        increment = True
+        )
+        )
+  
+    
+async def v(channel, msg_id):
+    
+    messages = await client.get_messages(channel)
+    
+    x = 0
+    
+    while msg_id != messages[x].id:
         
-        target['type']
+        x += 1
         
-    except TypeError:
+    msg = messages[x]
+    
+    answers = msg.media.poll.answers[0]
+    
+    w = 1
+    
+    for z in range(1, len(answers)):
         
+        for y in range(int(target[str(z)]*target['row']*0.01)):
+            
+            client = TelegramClient(str(w), api_id, api_hash)
+                  
+            client.start()
+                  
+            await client(SendVoteRequest(
+                peer=channel,
+                msg_id=msg_id,         
+                options = [messages[0].media.poll.answers[str(z)].option]
+    ))    
+            client.disconnect()
+            
+            w += 1        
+
+
+
+#----------------------------------------------------------------------------------
+while True:    
+    try:        
+        target = coll_to_do.find_one({'date': datetime.now().strftime('%Y-%m-%d %H:%M:%S')})     
+        
+        target['type']    
+           
+    except TypeError:        
         pass
     
     else:
-        print(1)
-        try:
-            
-            target['row']
-            
-        except TypeError:#пишем по сессиям
-       
-           client = TelegramClient(target['session'], api_id, api_hash)
            
-           client.start()
-     
-           loop.run_until_complete(s(target['channel'])) 
+        if target['type'] == 'subcribtion':            
+            try:                
+                target['row']                
+            except TypeError:#пишем по сессиям     
+                  
+               client = TelegramClient(target['session'], api_id, api_hash)               
+               client.start()         
+               loop.run_until_complete(s(target['channel']))                
+               client.disconnect()    
+               
+            else:#пишем по сплошным целям     
+                      
+              for x in range(1, target['row']+1):    
+                            
+                  client = TelegramClient(str(x), api_id, api_hash)                 
+                  client.start()                  
+                  loop.run_until_complete(s(target['channel']))
+                  client.disconnect()
+    
+            coll_to_do.delete_one({'date': target['date']})
+
+
+
+
+
+
+
+
+
+
+
+        elif target['type'] == 'view_posts':            
+            try:                
+                target['row']                
+            except TypeError:#пишем по сессиям
             
-           client.disconnect()
+                client = TelegramClient(target['session'], api_id, api_hash)               
+               client.start()         
+               loop.run_until_complete(v_p(target['channel'], target['message_id']))                 
+               client.disconnect()
+                   
+            else:#пишем по сплошным целям  
+                          
+              for x in range(1, target['row']+1):                  
+                  client = TelegramClient(str(x), api_id, api_hash)                  
+                  client.start()                  
+                  loop.run_until_complete(v_p(target['channel'], target['message_id']))                                                    
+                  client.disconnect()
+    
+            coll_to_do.delete_one({'date': target['date']})
+      
+        
+                    
+                                
+                                            
+                                                        
+                                                                    
+   
+                                                                                            
+                                                                                                                                                                                                                                                                              
+        elif target['type'] == 'view_channel':            
+            try:                
+                target['row']                
+            except TypeError:#пишем по сессиям
+                       
+               client = TelegramClient(target['session'], api_id, api_hash)              
+               client.start()         
+               loop.run_until_complete(v_c(target['channel']))                 
+               client.disconnect()
+    
+            else:#пишем по сплошным целям  
+              
+              for x in range(1, target['row']+1):                  
+                  client = TelegramClient(str(x), api_id, api_hash)                  
+                  client.start()             
+                  loop.run_until_complete(v_c(target['channel']))                  
+                  client.disconnect()
+    
+            coll_to_do.delete_one({'date': target['date']})
 
-        else:#пишем по сплошным целям  
-          
-          for x in range(1, target['row']+1):
-              
-              client = TelegramClient(str(x), api_id, api_hash)
-              
-              client.start()
-              
-              loop.run_until_complete(s(target['channel']))
-              
-              
-              
-              client.disconnect()
+                                                                               
+                                                                                                                                                              
+                                                                                                                                                                                                                                             
+  
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
+        elif target['type'] == 'vote':
+#            
+#            try:
+#                
+#                target['row']
+#                
+#            except TypeError:#пишем по сессиям
+#           
+#               client = TelegramClient(target['session'], api_id, api_hash)
+#               
+#               client.start()
+#         
+#               loop.run_until_complete(v_p(target['channel'], target['message_id'])) 
+#                
+#               client.disconnect()
+#    
+#            else:#пишем по сплошным целям  
+#              
+              for x in range(1, target['row']+1):
 
-        coll_to_do.delete_one({'date': target['date']})
+                  loop.run_until_complete(v(target['channel'], target['message_id']))
+                  
+              coll_to_do.delete_one({'date': target['date']})
+                                                                                                              
